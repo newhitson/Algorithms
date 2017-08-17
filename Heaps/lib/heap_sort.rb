36dca971d0f585ab4023514b991740e7decbd5bc
Nick <newhitson@gmail.com>
2017-08-17 11:41:35 -0700

    heap sort remove print

--- Heaps/lib/heap_sort.rb
+++ Heaps/lib/heap_sort.rb
@@ -8,15 +8,10 @@ class Array
         pointer += 1
         BinaryMinHeap.heapify_up(self, pointer, 0, &prc)
       end
-      p self
-
       until pointer == 0
 
         self[0], self[pointer] = self[pointer], self[0]
-        p self
         BinaryMinHeap.heapify_down(self, 0, pointer, &prc)
-        p self
-        p "exec"
         pointer -= 1
 
       end
