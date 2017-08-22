c9484a8c3722a050d07017eba168703adaaa1efe
Nick <newhitson@gmail.com>
2017-08-21 20:35:41 -0700

    add graph and topological sort

--- graphs_and_topological_sort/lib/graph.rb
+++ graphs_and_topological_sort/lib/graph.rb
@@ -1,15 +1,36 @@
 class Vertex
   def initialize(value)
-
+    @value = value
+    @in_edges = []
+    @out_edges = []
   end
+  attr_accessor :value, :in_edges, :out_edges
 end
 
 class Edge
   def initialize(from_vertex, to_vertex, cost = 1)
 
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+
+    @cost = cost
+
+    @to_vertex.in_edges << self
+    @from_vertex.out_edges << self
+
   end
 
-  def destroy!
+  attr_accessor :cost, :to_vertex, :from_vertex, :destination
 
+  def destroy!
+    @from_vertex.out_edges.delete(self)
+
+    @to_vertex.in_edges.delete(self)
+
+    @from_vertex, @to_vertex = nil
+
+  end
+
+  def destination
   end
 end
--- graphs_and_topological_sort/lib/topological_sort.rb
+++ graphs_and_topological_sort/lib/topological_sort.rb
@@ -3,5 +3,31 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  sorted = []
+  top = []
 
+  vertices.each do |vertx|
+    if vertx.in_edges.empty?
+      top << vertx
+    end
+  end
+
+  until top.empty?
+    current = top.pop
+    sorted << current
+
+    edges = current.out_edges.dup
+
+    edges.each do |edge|
+      destination = edge.to_vertex
+      edge.destroy!
+      if destination.in_edges.empty?
+        top << destination
+      end
+
+    end
+
+  end
+   return sorted if sorted.length == vertices.length
+   []
 end
